7d43bea7810adb0a054404bb68d3652ef586d471
Joel Arvidsson <joel@oblador.se>
2015-01-05 14:16:08 +0800

    Added plural tests for dust.js

--- test/extract_plurals.js
+++ test/extract_plurals.js
@@ -18,6 +18,23 @@ describe('Extracting plurals', function () {
         assert.deepEqual(i[0].msgstr, ['', '']);
     });
 
+    it('works on Dust.js', function () {
+        var files = [
+            'test/fixtures/plural.dust'
+        ];
+        var catalog = testExtract(files);
+
+        var i = catalog.items;
+        assert.equal(i.length, 2);
+
+        assert.equal(i[0].msgid, 'Bird');
+        assert.equal(i[0].msgid_plural, 'Birds');
+        assert.deepEqual(i[0].msgstr, ['', '']);
+
+        assert.equal(i[1].msgid, 'You have one bird.');
+        assert.equal(i[1].msgid_plural, 'You have {birds.length} birds.');
+    });
+
     it('merges singular and plural strings', function () {
         var files = [
             'test/fixtures/merge.html'
